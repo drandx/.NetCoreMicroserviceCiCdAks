337c85f0856ab8e57aeb2594ae098718abd95a74
Juan Garcia <juangarcia@microsoft.com>
2021-07-07 15:59:18 -0700

    Added new folder structure with new webjob

---
+++ WebJobs/Continuous/CustomerApi.WebJobs.WebJobOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CustomerApi.WebJobs.WebJobOne
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+             Console.WriteLine("Hello World!");
+             var builder = new HostBuilder();
+             builder.UseEnvironment(EnvironmentName.Development);
+             builder.ConfigureWebJobs(b =>
+             {
+                 b.AddAzureStorageCoreServices();
+             });
+            builder.ConfigureLogging(b =>
+            {
+                b.AddConsole();
+            });
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            var host = builder.Build();
+            using (host)
+             {
+                 await host.RunAsync();
+             }
+        }
+    }
+    }
