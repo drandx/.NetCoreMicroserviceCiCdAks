030f9ee413e462fbef929c96a31328c68b424f52
Juan Garcia <juangarcia@microsoft.com>
2021-07-07 17:22:50 -0700

    Added webjob two

---
+++ WebJobs/Continuous/CustomerApi.WebJobs.WebJobTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomerApi.WebJobs.WebJobTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
