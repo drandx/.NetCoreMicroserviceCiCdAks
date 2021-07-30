2e642ea1368e620adf08c6dc1917f3cca5ab120c
Juan Garcia <juangarcia@microsoft.com>
2021-07-30 08:18:34 -0700

    Added exclude to restore

---
+++ FailingProj/Program.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+
+namespace FailingProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
